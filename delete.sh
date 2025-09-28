0f271e2c3c451d68c2d78d0b300a24addc9b9388
SureshU <sureshdevops@gmail.com>
2025-09-28 15:36:12 +0530

    message

--- delete.sh
+++ delete.sh
@@ -6,14 +6,15 @@ logfile="$path/$file.log"
 mkdir -p $path
 
 if [ ! -d $path ]; then
-echo "directory doesn't exist"
+echo "$path doesn't exist"
+exit 1
 fi
 
 filestodelete=$(find $path -name "*.log")
-while IFS=read filepath 
+while IFS= read -r filepath 
 do 
        echo "Delete the file: $filepath"
        rm -rf $filepath
        echo "Deleted the file: $filepath"
 
-done <<<$filestodelete
+done <<< $filestodelete
