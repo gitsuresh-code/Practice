d5d9444587239ddad4e707a5f5ef22ff0250ca48
SureshU <sureshdevops@gmail.com>
2025-09-28 15:09:02 +0530

    message

--- delete.sh
+++ delete.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 path="/var/log/scriptlog/"
 file=$(echo $0 | cut -d "." -f1)
-logfile=$path/$file.log
+logfile="$path/$file.log"
+
+mkdir -p $path
 
 filepath=$(find . -name "*.log" -type f)
-while [ IFS=read -s $filepath ]
+while IFS=read $filepath 
 do 
        echo "Delete the file: $filepath"
        rm -rf $filepath
