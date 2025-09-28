11241f615b41403dc8be9131ce2cdc69d4b75ef9
SureshU <sureshdevops@gmail.com>
2025-09-28 18:36:56 +0530

    message

---
+++ backup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+user=$(id -u) #user check
+
+#colog codes
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+sourced=$1 # first argument
+destd=$2   # second argument
+days=${3:-14} # if not provided considered as 14 days
+
+
+path="/tmp/shell"
+name=$( echo $0 | cut -d "." -f1 )
+logpath="$path/$name.log" # /tmp/shell/backup.log
+
+mkdir -p $path
+echo "Script started executed at: $(date)" | tee -a $logpath
+
+if [ $user -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # if user is not root. script will end here
+fi
+
+usage_instruction(){
+    echo -e "$R USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days] $N"
+    exit 1 #if 2 args are not passed. script will end here
+}
+
+### Check for Args passed ####
+if [ $# -lt 2 ]; then
+    usage_instruction
+fi
+
+### Check SOURCE_DIR Exist ####
+if [ ! -d $sourced ]; then
+    echo -e "$R Source $sourced does not exist $N"
+    exit 1 #script will end here
+fi
+
+### Check DEST_DIR Exist ####
+if [ ! -d $destd ]; then
+    echo -e "$R Destination $destd does not exist $N"
+    exit 1 #script will end here
+fi
+
+### Find the files older than X days ####
+
+files=$(find $sourced -name "*.log" -type f -mtime +$days)
+
+if [ -n "$files" ]; then
+    ### Start Archeiving ###
+    echo "Files found: $files"
+    timestamp=$(date +%F-%H-%M)
+    zipfile="$destd/app-logs-$timestamp.zip"
+    echo "Zip file name: $zipfile"
+
+    find $sourced -name "*.log" -type f -mtime +$days | zip -@ -j "$zipfile"
+
+    ### Check Archieval Success or not ###
+    if [ -f $zipfile ]
+    then
+        echo -e "Archeival ... $G SUCCESS $N"
+
+        ### Delete if success ###
+        while IFS= read -r filepath
+        do
+            echo "Deleting the file: $filepath"
+            rm -rf $filepath
+            echo "Deleted the file: $filepath"
+        done <<< $files
+    else
+        echo "Archieval ... $R FAILURE $N"
+        exit 1
+    fi
+else
+    echo -e "No files to archeive ... $Y SKIPPING $N"
+fi
